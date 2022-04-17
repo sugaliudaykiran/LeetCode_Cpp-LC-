367e06506f10c6c5b9c6d8162b7f835f40518127
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-04-17 22:55:09 +0530

    Time: 0 ms (100.00%), Space: 8 MB (45.48%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector <TreeNode *> res;
+    TreeNode* increasingBST(TreeNode* root) {
+        if (root == NULL){
+            return NULL;
+        }
+        inorder(root);
+        
+        for (int i=0; i<res.size()-1; i++){
+            res[i] -> right = res[i+1];
+            res[i] -> left = NULL;
+        }
+        res[res.size()-1] -> right = NULL;
+        res[res.size()-1] -> left = NULL;
+        
+        return res[0];
+    }
+    void inorder(TreeNode *node){
+        if (node == NULL){
+            return;
+        }
+        inorder(node -> left);
+        res.push_back(node);
+        inorder(node -> right);
+    }
+};
+
+/*      Navie Solution :- Using vector to store the items and makes the right skewed tree.
+            TC -> O(n).. AS -> O(n).. where n is no of nodes in the tree..
+        
+*/
