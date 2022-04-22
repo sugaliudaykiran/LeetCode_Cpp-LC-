12c0866205ef2e9fa2fd1de3a0b42d6a41e47b68
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-04-22 23:21:14 +0530

    Time: 1395 ms (5.02%), Space: 338.2 MB (5.02%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+public:
+    vector <int> v;     //  Navie Solution :-   By using addition vector to store and the key and value.. pairs..
+    int size;
+    
+    MyHashMap() {
+        size = 10e6 +1;
+        v.resize(size);
+        fill(v.begin(), v.end(), -1);
+    }
+    
+    void put(int key, int value) {
+        v[key] = value;   
+    }
+    
+    int get(int key) {
+        return v[key];
+    }
+    
+    void remove(int key) {
+        v[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
