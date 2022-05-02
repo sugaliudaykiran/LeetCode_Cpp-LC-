b895dc9c0c817884b1fbf73942f4dca269c2bc85
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-05-02 20:04:55 +0530

    Time: 17 ms (35.77%), Space: 16.2 MB (57.28%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -1,35 +1,14 @@
 class Solution {
-private:
-    bool isOdd(int n)
-    {
-        if (n % 2 != 0)
-            return true;
-        else
-            return false;
-    }
-    
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int p1 = 0, p2 = nums.size()-1;
-        while (p1 < p2)
+        int j = 0;
+        
+        for (int i=0; i<nums.size(); i++)
         {
-            if (isOdd(nums[p1]) and !isOdd(nums[p2]))
+            if (nums[i]%2 == 0)
             {
-                swap(nums[p1], nums[p2]);
-                p1++; p2--;
-            }
-            if (!isOdd(nums[p1]) and !isOdd(nums[p2]))
-            {
-                p1++;
-            }
-            else if (isOdd(nums[p1]) and isOdd(nums[p2]))
-            {
-                p2--;
-            }
-            else if (!isOdd(nums[p1]) and isOdd(nums[p2]))
-            {
-                p1++;
-                p2--;
+                swap(nums[j], nums[i]);
+                j++;
             }
         }
         return nums;
