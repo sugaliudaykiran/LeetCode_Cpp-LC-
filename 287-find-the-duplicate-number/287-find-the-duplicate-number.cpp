c1a471c7e60fd039d9c2ad8174398267e594580b
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-06-09 12:50:55 +0530

    Time: 174 ms (48.93%), Space: 63.6 MB (23.50%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        vector <int> hash(n, 0);
+        for (int i=0; i<n; i++)
+        {
+            hash[nums[i]]++;
+        }
+        for (int i=0; i<n; i++)
+        {
+            if (hash[i] >= 2)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+
+/*  
+                                    Navie Approach :- 
+    1) First sort the vector and check each item with next item, if condition is true then there is a  duplicate number..
+    2) TC -> O(n logn).. using merge sort or any sort function..     SC -> O(1)
+    
+        sort(nums.begin(), nums.end());     
         
         for (int i=0; i<nums.size(); i++)
         {
@@ -11,5 +34,6 @@ public:
             }
         }
         return 0;
-    }
-};
+                                    
+                                    Using HashTable :-
+*/                                     
