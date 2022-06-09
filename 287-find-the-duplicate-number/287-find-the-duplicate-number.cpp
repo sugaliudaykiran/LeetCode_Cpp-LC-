2f68f6411b0868b43a05ae4efacd42ddb4c1a2c0
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-06-09 16:33:17 +0530

    Time: 148 ms (65.90%), Space: 61.3 MB (61.34%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n = nums.size();
+    
+        int slow = nums[0];          // Both slow and fast were starts from first element
+        int fast = nums[0];          //  In this we move based on value..
+        do{
+            slow = nums[slow];              //  slow move one step ahead..
+            fast = nums[nums[fast]];        //  Fast moves two steps ahead..
+        }while (slow != fast);
         
-        vector <int> hash(n, 0);
-        for (int i=0; i<n; i++)
+        fast = nums[0];
+        while (slow != fast)
         {
-            hash[nums[i]]++;
+            slow = nums[slow];
+            fast = nums[fast];      //  Both pointers are moved one step..
         }
-        for (int i=0; i<n; i++)
-        {
-            if (hash[i] >= 2)
-            {
-                return i;
-            }
-        }
-        return 0;
+        return slow;
     }
 };
 
@@ -25,7 +25,6 @@ public:
     2) TC -> O(n logn).. using merge sort or any sort function..     SC -> O(1)
     
         sort(nums.begin(), nums.end());     
-        
         for (int i=0; i<nums.size(); i++)
         {
             if (nums[i] == nums[i+1])
@@ -36,4 +35,25 @@ public:
         return 0;
                                     
                                     Using HashTable :-
-*/                                     
+    1) Create a frequency (or) count vector of size 'n' and initialize the value as zero..
+    2) Traverse the nums and increment the value count by '1', in the count vector of that [nums as that index]..
+    3) Check any count has more than 2 (or) equal to 2.. then return the Index of count vector.. which is an element has duplicate..
+    4)  TC -> O(n)..    SC -> O(n).. Using additional vector..
+        
+        int n = nums.size();
+        vector <int> hash(n, 0);
+        for (int i=0; i<n; i++)
+        {
+            hash[nums[i]]++;
+ 
+            if (hash[i] >= 2)
+            {
+                return i;
+            }
+        }    
+        return 0;
+                                    Using Two Pointers :-   [Slow and Fast Pointers..]
+                                    
+        
+    
+*/             
