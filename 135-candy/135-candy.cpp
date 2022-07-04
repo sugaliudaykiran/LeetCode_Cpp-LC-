79b3fb308a34f9e1571f292b026fd7145f1d1ee0
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-05 00:00:32 +0530

    Time: 37 ms (40.28%), Space: 18.1 MB (33.66%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -7,25 +7,25 @@ public:
         vector <int> left(n, 1);
         vector <int> right(n, 1);
     
-        for (int i=1; i<n; i++)
+        for (int i=1; i<n; i++)         //  checking the left neighbours..
         {
             if (ratings[i] > ratings[i-1])
             {
-                left[i] = left[i-1] +1;
+                left[i] = left[i-1] +1;         //  if cur is greater than left_neighbours then give more..
             }
         }
         
-        for (int j=n-2; j>=0; j--)
+        for (int j=n-2; j>=0; j--)      //  checking the right neighbours..
         {
             if (ratings[j] > ratings[j+1])
             {
-                right[j] = right[j+1] +1;
+                right[j] = right[j+1] +1;      //  if cur is greater than right_neighbours then give more..
             }
         }
         
         for (int k=0; k<n; k++)
         {
-            res += max(left[k], right[k]);
+            res += max(left[k], right[k]);      //  Finally does max of it's both neighbours..
         }
         return res;
     }
