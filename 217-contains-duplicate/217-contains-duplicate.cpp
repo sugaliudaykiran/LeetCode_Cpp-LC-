3a4530233eb53b8afb9571e764a48bda9c3eec1b
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-05 21:37:10 +0530

    Time: 213 ms (7.95%), Space: 54.9 MB (6.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,8 +1,43 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        int set_size = set <int>(nums.begin(), nums.end()).size();  //  TC -> O(1).. SC -> O(n)..
         
-        sort(nums.begin(), nums.end());
+        if (set_size != nums.size())
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+
+/*
+        
+        
+    bool containsDuplicate(vector<int> &nums) {
+            unordered_map <int, int> mp;        //  TC -> O(n).. SC -> O(n)..
+            
+            for (int i=0; i<nums.size(); i++)
+            {
+                mp[nums[i]]++;
+            }
+        
+            for (int i=0; i<nums.size(); i++)
+            {
+                if (mp[nums[i]] > 1)
+                {
+                    return true;
+                }
+                
+            }
+        return false;
+    }
+
+    
+    bool containsDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());         //  TC -> O(NlogN).. SC -> O(1)..
         
         for (int i=1; i<nums.size(); i++)
         {
@@ -13,4 +48,7 @@ public:
         }
         return false;
     }
-};
+
+
+
+*/ 
