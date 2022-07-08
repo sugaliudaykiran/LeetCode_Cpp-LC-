10edcd6c07afac423b0db7ebd84218f4de87b7c1
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-08 23:21:13 +0530

    Time: 119 ms (60.90%), Space: 10.8 MB (77.13%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,92 +1,55 @@
-int n;
-vector<int> houses;
-vector<vector<int>> cost;
-vector<vector<vector<int>>> dp;
-int c;
-int rec(int i, int last, int target) {
-    // Pruning
-    if(target < 0 ) return -1; // we cant make negative target, also we can't access dp[][][-veTarget] so prune it initially.
-	
-    // cout<<"Checking Base case : "<<endl;
-    // base case
-    if(i==n && target!=0) { // it can be so that we have not made required targets.
-        return -1;
-    }
-    if(i==n && target==0) {
-        return 0;
-    }
-    
-    // cache check
-    // cout<<"Checking Cache : "<<endl;
-    
-    if(dp[i][last][target]!=-2) {
-        return dp[i][last][target];
-    }
-    // cout<<"Cache not found"<<endl;
-	
-    int ans=INT_MAX; // keeping answer as INT_MAX because we will take minimum with possible answer.
-    bool possible=false; // No color is possible till now 
-	
-    // cout<<"Checking if house already colored "<<endl;
-    if(houses[i]!=0) {
-        if(houses[i]!=last) {
-            return dp[i][last][target]=rec(i+1,houses[i],target-1);
-        }
-        else {
-            return dp[i][last][target]=rec(i+1,houses[i],target);
-            }
-    }
-    // cout<<"House Not colored last summer"<<endl;
-	
-    // cout<<"Trying all colors : "<<endl;
-    for(int k=0;k<c;k++) {
-        // choice of color is k+1
-        // index is k
-        // cout<<"Trying color : "<<k+1<<endl;
-        int temp;
-        if(k+1 != last) {
-         temp=rec(i+1,k+1,target-1);        
-        }
-        else {
-            temp=rec(i+1,k+1,target);        
-        }
-        // if this color is possible
-        if(temp!=-1) {
-            // cout<<k+1<<" color is possible"<<endl;
-            possible=true;
-            ans=min(ans,cost[i][k]+temp);
-        }
-    }
-    if(possible) {
-    return dp[i][last][target]=ans;
-                
-    }
-    else { // no possible color found for this place, as we need to color all houses, => answer is -1.
-        return dp[i][last][target]=-1;
-    }
-    
-}
-
 class Solution {
 public:
-    int minCost(vector<int>& house, vector<vector<int>>& cst, int r, int col, int target) {
-        cost=cst;
-        houses=house;
-        c=col;
-        n=r;
-		dp.resize(n+1);
-        for(int i=0;i<=n;i++) {
-            dp[i].resize(n+1);
-            for(int j=0;j<=n;j++) {
-                dp[i][j].resize(target+1);
-                for(int k=0;k<=target;k++) {
-                    dp[i][j][k]=-2;
-                }
+    // Assign the size as per maximum value for different params
+    int memo[100][100][21];
+    // Maximum cost possible plus 1
+    const int MAX_COST = 1000001;
+    
+    int findMinCost(vector<int>& houses, vector<vector<int>>& cost, int targetCount, int currIndex,
+                    int neighborhoodCount, int prevHouseColor) {
+        if (currIndex == houses.size()) {
+            // If all houses are traversed, check if the neighbor count is as expected or not
+            return neighborhoodCount == targetCount ? 0 : MAX_COST;
+        }
+        
+        if (neighborhoodCount > targetCount) {
+            // If the neighborhoods are more than the threshold, we can't have target neighborhoods
+            return MAX_COST;
+        }
+        
+        // We have already calculated the answer so no need to go into recursion
+        if (memo[currIndex][neighborhoodCount][prevHouseColor] != -1) {
+            return memo[currIndex][neighborhoodCount][prevHouseColor];
+        }
+        
+        int minCost = MAX_COST;
+        // If the house is already painted, update the values accordingly
+        if (houses[currIndex]) {
+            int newNeighborhoodCount = neighborhoodCount + (houses[currIndex] != prevHouseColor);
+            minCost = 
+                findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, houses[currIndex]);
+        } else {
+            int totalColors = cost[0].size();
+            
+            // If the house is not painted, try every possible color and store the minimum cost
+            for (int color = 1; color <= totalColors; color++) {
+                int newNeighborhoodCount = neighborhoodCount + (color != prevHouseColor);
+                int currCost = cost[currIndex][color - 1]
+                    + findMinCost(houses, cost, targetCount, currIndex + 1, newNeighborhoodCount, color);
+                minCost = min(minCost, currCost);
             }
         }
-		//dp.resize(m+1 , vector<vector<int>>(n+1 , vector<int>(target+1 , -2));
-        // cout<<"initialised: "<<endl;
-        return rec(0,0,target); // hope nobody acceses -1 index in dp
-        // return -1;
+        
+        // Return the minimum cost and also storing it for future reference (memoization)
+        return memo[currIndex][neighborhoodCount][prevHouseColor] = minCost;
+    }
+    
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        // Marking all values to -1 to denote that we don't have the answer ready for these params yet
+        memset(memo, -1, sizeof(memo));
+        int answer = findMinCost(houses, cost, target, 0, 0, 0);
+        
+        // Return -1 if the answer is MAX_COST as it implies no answer possible
+        return answer == MAX_COST ? -1 : answer;
     }
 };
