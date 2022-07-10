fc2d460b0ad2e2f4203297fdd0491dd26e9387bb
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-10 21:46:34 +0530

    Time: 3 ms (97.24%), Space: 13.7 MB (51.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
+        int n = cost.size();    //  TC -> O(n).. SC -> O(1)..
         
-        cost.push_back(0);
+        cost.push_back(0);  //  ADD a nth item because to reach top of floor.. it takes 0 time..
         
-        for (int i=n-2; i>=0; i--)
+        for (int i=n-2; i>=0; i--)  //  traverse from right to left [which can't be 'n and n-1' case..]
         {
-            cost[i] += min(cost[i+1], cost[i+2]);
+            cost[i] += min(cost[i+1], cost[i+2]);   // find min of both it's next by 1 and next by 2 steps..
         }
-        return min(cost[0], cost[1]);
+        return min(cost[0], cost[1]);  // return min of first two steps.. it required result.
     }
 };
