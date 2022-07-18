78eb63c7ba95d3ca0b83114d779dbaf5af5561cb
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-19 00:08:01 +0530

    Time: 182 ms (22.05%), Space: 41.5 MB (45.21%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -3,17 +3,17 @@ public:
      int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         
-        int prefix[n];
+        int prefix[n];  //  Find the prefix_sum of the given nums..
         prefix[0] = nums[0];
         
         for (int i=1; i<n; i++)
         {
-            prefix[i] = nums[i] + prefix[i-1];
+            prefix[i] = nums[i] + prefix[i-1];  // By using cur_i_th in nums + prefix[i-1] th one..
         }
         
-        int count = 0;
+        int count = 0;   // keep count of subarray sum's equal to k..
         unordered_map <int, int> mp;
-        
+        //      use the map to find that (prefix[i] + prefix[j] = k).. so (prefix[i] - k)..
         for (int i=0; i<n; i++)
         {
             if (prefix[i] == k)
@@ -23,9 +23,9 @@ public:
             
             if (mp.find(prefix[i] - k) != mp.end())
             {
-                count += mp[prefix[i] - k];
+                count += mp[prefix[i] - k]; //  If, found, then increment it val to count..
             }
-            mp[prefix[i]]++;
+            mp[prefix[i]]++;    //  Insert to the map..
         }
         return count;
 	};
