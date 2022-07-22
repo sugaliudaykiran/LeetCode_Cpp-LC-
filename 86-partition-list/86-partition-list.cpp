d5737d33644579cc6cb2cac95dfe7e988a3ec21c
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-22 23:57:34 +0530

    Time: 13 ms (23.02%), Space: 10.3 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -1,74 +1,38 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if (head == NULL || head -> next == NULL)
+        ListNode *fdum = new ListNode(0), *bdum = new ListNode(0);
+        
+        ListNode *front = fdum, *back = bdum, *cur = head;
+        
+        while (cur != NULL)     //  TC -> O(n).. SC -> O(1)
         {
-            return head;
-        }
-        
-        ListNode *h1 = new ListNode(0);
-        ListNode *h2 = new ListNode(0);
-        
-        ListNode *temp = head, *p1 = NULL, *p2 = NULL;
-        
-        while (temp != NULL)
-        {
-            if (temp -> val < x)
+            if (cur -> val < x)
             {
-                if (p1 == NULL)
-                {
-                    h1 -> next = temp;
-                    p1 = temp;
-                }
-                else 
-                {
-                    p1 -> next = temp;
-                    p1 = p1 -> next;
-                }
+                front -> next = cur;
+                front = cur;
             }
-            
-            else
+            else 
             {
-                if (p2 == NULL)
-                {
-                    h2 -> next = temp;
-                    p2 = temp;
-                }
-                else 
-                {
-                    p2 -> next = temp;
-                    p2 = p2 -> next;
-                }
+                back -> next = cur;
+                back = cur;
             }
-            temp = temp -> next;
+            cur = cur -> next;
         }
-        if (h1 -> next == NULL && h2 -> next)
-        {
-            p2 -> next =  NULL;
-            return h2 -> next;
-        }
-        else if (h2 -> next == NULL && h1 -> next)
-        {
-            p1 -> next =  NULL;
-            return h1 -> next;
-        }
-        else if (h2 -> next && h1 -> next)
-        {
-            p1 -> next = NULL;
-            p2 -> next = NULL;
-            p1 -> next = h2 -> next;
-            return h1 -> next;
-        }
-        return head;
+        front -> next = bdum -> next;
+        back -> next = nullptr;
+        return fdum -> next;
     }
 };
+
+
+/*
+        Idea:
+    The easiest thing to do here is to create separate linked lists for the front and back portions of list we want to return. In order to accomplish that, we should first create some dummy heads (fdum, bdum), then create pointers for the current nodes each of the front, back, and main lists (front, back, curr).
+
+Then we can simply iterate through the main list and stitch together each node to either front or back, depending on the node's value.
+
+Once we reach the end, we just need to stitch together the two sub-lists, making sure to cap off the end of back, and then return our new list, minus the dummy head.
+
+*/
+
