fed3218bce5f5390732688f2926d0779850028fc
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-07-29 23:54:21 +0530

    Time: 11 ms (23.78%), Space: 8.5 MB (43.97%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string p) {        
-	vector<string> ans;
-	for(auto& w : words) 
-		if(match(w, p)) ans.push_back(w);
-	return ans;
-}
-bool match(string& w1, string& w2) {
-	unordered_map<char, char> w1_to_w2, w2_to_w1;
-	for(int i = 0; i < size(w1); i++) 
-		if( (w1_to_w2.count(w1[i]) && w1_to_w2[w1[i]] != w2[i]) || 
-			(w2_to_w1.count(w2[i]) && w2_to_w1[w2[i]] != w1[i]) ) 
-			return false; 
-		else
-			w1_to_w2[w1[i]] = w2[i],
-			w2_to_w1[w2[i]] = w1[i];
-	return true;
-}
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector <string> res;
+        
+        for (auto &word: words)
+        {
+            if (match(word, pattern))
+            {
+                res.push_back(word);
+            }
+        }
+        return res;
+    }
+    
+    
+    bool match(string &word, string &pattern)
+    {
+        unordered_map <char, char> word_toPat, pat_toWord;
+        
+        for (int i = 0; i <pattern.size(); i++)
+        {
+            if (word_toPat[word[i] - 'a'] == 0)
+            {
+                word_toPat[word[i] - 'a'] = pattern[i];
+            }
+            if (pat_toWord[pattern[i] - 'a'] == 0)
+            {
+                pat_toWord[pattern[i] - 'a'] = word[i];
+            }
+            
+            if (word_toPat[word[i] - 'a'] != pattern[i] || pat_toWord[pattern[i] - 'a'] != word[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 };
