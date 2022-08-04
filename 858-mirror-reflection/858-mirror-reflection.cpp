fac239af35e983f99a8eb96e32d9b463ec99a56e
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-08-05 00:25:24 +0530

    Time: 3 ms (36.36%), Space: 6 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
+    // extension * p = reflection * q..
     int mirrorReflection(int p, int q) {
-        while (p % 2 == 0 && q % 2 == 0){ 
-            p/=2;
+        int extension = q, ref = p;
+        
+        while (p % 2 == 0 && q % 2 == 0)
+        {
+            p/= 2;
             q/=2;
         }
-        return 1 - p % 2 + q % 2;
+        if (p % 2 == 0 && q % 2 != 0)
+        {
+            return 2;
+        }
+        else if (p % 2 != 0 && q % 2 != 0)
+        {
+            return 1;
+        }
+        else if (p % 2 != 0 && q % 2 == 0)
+        {
+            return 0;
+        }
+        return -1;
     }
 };
