03d09852624af21bb4acb211a107496f81aa905f
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-08-07 00:11:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
-
-int poorPigs(int buckets, int poisonTime, int totalTime) {
-    return ceil(log(buckets) / log(totalTime / poisonTime + 1));
-}
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        //  Finding base condition.. that for base 5..adding up..count of poorpig.
+        
+        int poorpig = 0;
+        while (true)
+        {
+            int x = (int )(minutesToTest/minutesToDie)+1;
+            if (pow(x, poorpig) < buckets)
+                poorpig++;
+            else
+                break;
+        }
+        return poorpig;
+    }
 };
