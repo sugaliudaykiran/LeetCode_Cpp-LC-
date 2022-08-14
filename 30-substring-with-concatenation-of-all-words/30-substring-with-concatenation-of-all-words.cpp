5d8dff691b6118a6934df1c85dc83ba158664488
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-08-14 23:35:00 +0530

    Time: 1415 ms (22.57%), Space: 339.6 MB (17.09%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,58 +1,38 @@
 class Solution {
 public:
-     // travel all the words combinations to maintain a window
-    // there are wl(word len) times travel
-    // each time, n/wl words, mostly 2 times travel for each word
-    // one left side of the window, the other right side of the window
-    // so, time complexity O(wl * 2 * N/wl) = O(2N)
-    vector<int> findSubstring(string S, vector<string> &L) {
-        vector<int> ans;
-        int n = S.size(), cnt = L.size();
-        if (n <= 0 || cnt <= 0) return ans;
-        
-        // init word occurence
-        unordered_map<string, int> dict;
-        for (int i = 0; i < cnt; ++i) dict[L[i]]++;
-        
-        // travel all sub string combinations
-        int wl = L[0].size();
-        for (int i = 0; i < wl; ++i) {
-            int left = i, count = 0;
-            unordered_map<string, int> tdict;
-            for (int j = i; j <= n - wl; j += wl) {
-                string str = S.substr(j, wl);
-                // a valid word, accumulate results
-                if (dict.count(str)) {
-                    tdict[str]++;
-                    if (tdict[str] <= dict[str]) 
-                        count++;
-                    else {
-                        // a more word, advance the window left side possiablly
-                        while (tdict[str] > dict[str]) {
-                            string str1 = S.substr(left, wl);
-                            tdict[str1]--;
-                            if (tdict[str1] < dict[str1]) count--;
-                            left += wl;
-                        }
-                    }
-                    // come to a result
-                    if (count == cnt) {
-                        ans.push_back(left);
-                        // advance one word
-                        tdict[S.substr(left, wl)]--;
-                        count--;
-                        left += wl;
-                    }
+    
+    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+        for(int j=0; j<s.size(); j+=wordLen) {
+            string w = s.substr(j, wordLen);
+            if(wordCount.find(w) != wordCount.end()) {
+                if(--wordCount[w] == -1) {
+                    return false;
                 }
-                // not a valid word, reset all vars
-                else {
-                    tdict.clear();
-                    count = 0;
-                    left = j + wl;
-                }
+            } else {
+                return false;
             }
         }
+        return true;
+    }
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        int wordLen = words[0].size();
+        int sLen = s.size();
+        int wordsWindow = words.size() * wordLen;
         
-        return ans;
+        unordered_map<string, int> wordCount;
+        for(int i=0; i<words.size(); i++) {
+            wordCount[words[i]]++;
+        }
+        
+        int i = 0;
+        while(i + wordsWindow <= sLen) {
+            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
+                res.push_back(i);
+            }
+            i++;
+        }
+        return res;
     }
 };
