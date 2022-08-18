ddbb07500179fdbda68b6258297fc2c0522cd470
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-08-19 00:24:22 +0530

    Time: 367 ms (44.40%), Space: 77.5 MB (87.80%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,38 +1,38 @@
-class Solution {
-public:
-    int minSetSize(vector<int>& arr) {
-        int n = arr.size();
-        
-        unordered_map <int, int> cnt;
-        for (auto x: arr)
-        {
-            ++cnt[x];
+class Solution
+{
+    public:
+        int minSetSize(vector<int> &arr) {
+            int n = arr.size();
+            
+            unordered_map <int, int> cnt;
+            for (auto x: arr)
+            {
+                ++cnt[x];
+            }
+            
+            vector <int> counting(n+1);
+            for (auto [_, freq]: cnt)
+            {
+                ++counting[freq];
+            }
+            int ans = 0, removed = 0, half = n/2, freq = n;
+            while (removed < half)
+            {
+                ans+=1;
+                while (counting[freq] == 0)
+                {
+                    --freq;
+                }
+                
+                removed += freq;
+                --counting[freq];
+            }
+            return ans;
         }
-        
-        vector <int> frequency;
-        for (auto [_, freq]: cnt)
-        {
-            frequency.push_back(freq); 
-        }
-        sort(frequency.begin(), frequency.end());
-        
-        int res = 0, removed = 0, half = n/2 ,i = frequency.size()-1;
-        
-        while (removed < half)
-        {
-            res+=1;
-            removed += frequency[i--];
-        }
-        return res;
-    }
 };
 
-
-/*      
-    Using Hashing and Sorting..   TC -> O(NLogN). SC -> O(N).
-    1) Using Hashmap to count the freq of each number..
-    2) Get the freq into the frequency array..
-    3) sort the frequency Increasing order..
-    4) Our prioritize removing the largest frequency first and we've eliminated at least half of the arr..
-    5) return no of numbers removed.
-*/ 
+/*
+    Using Hashmap and Counting sort..
+    1) In worst case, Maximum value in frequencies array is N, when all numbers are same..
+    2) We sort frequencies, by using counting sort, which is O(N).. in time complexity..
+        */
