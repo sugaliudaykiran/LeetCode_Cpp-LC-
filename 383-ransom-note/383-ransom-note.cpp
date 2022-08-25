df9068cf35c534ac6471a2afce20d146595132b4
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-08-25 07:37:14 +0530

    Time: 18 ms (74.92%), Space: 8.9 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
          vector <int> freq(27, 0);
-        for (int i=0; i<magazine.size(); i++)
+        for (int i=0; i<magazine.size(); i++) // TC -> O(M+N).. SC -> O(M)
         {
             ++freq[magazine[i] - 'a'];
         }
