f3fe63d3a4da3d5e82df0215350e48cb2ca4aa10
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-08-29 00:06:55 +0530

    Time: 13 ms (77.01%), Space: 9.4 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-   vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        // all elements on same diagonal have same i-j result.
-        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map; // min priority queue
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                map[i - j].push(mat[i][j]);
+        // same diagonal have same value of i - j
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> mp; // min_heap..
+        for (int i=0; i<m; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                mp[i-j].push(mat[i][j]);
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = map[i - j].top(); map[i - j].pop();
+        
+       for (int i=0; i<m; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                mat[i][j] = mp[i-j].top();
+                mp[i-j].pop();
             }
         }
         return mat;
     }
-
 };
+
+/*
+    Explanation
+    A[i][j] on the same diagonal have same value of i - j
+    For each diagonal,
+    put its elements together, sort, and set them back.
+
+
+    Complexity
+    Time O(MNlogD), where D is the length of diagonal with D = min(M,N).
+    Space O(MN)
+*/ 
