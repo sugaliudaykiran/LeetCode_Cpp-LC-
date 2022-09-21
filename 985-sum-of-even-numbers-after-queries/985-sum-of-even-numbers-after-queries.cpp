373f0ddf86488370a7745f2b75c56357b7b67dd1
sugaliudaykiran <70463117+sugaliudaykiran@users.noreply.github.com>
2022-09-21 22:30:50 +0530

    Time: 256 ms (20.83%), Space: 46.1 MB (38.69%) - LeetHub

---
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+        vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& qs, vector<int> res = {}) {
+      int sum = accumulate(begin(A), end(A), 0, [](int s, int a) { return s + (a % 2 == 0 ? a : 0); });
+      for (auto &q : qs) {
+        if (A[q[1]] % 2 == 0) sum -= A[q[1]];
+        A[q[1]] += q[0];
+        if (A[q[1]] % 2 == 0) sum += A[q[1]];
+        res.push_back(sum);
+      }
+      return res;
+    }
+};
+
+/*
+
+    First, we calculate sum of even numbers. Then, for each query, if A[index] is even, we subtract its value from sum. Then we add val to A[index]. If A[index] is now even, we add its value to sum.
+
+
+*/
